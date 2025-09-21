69c11dcd2c12612b0b1a35335892e56123452c45
Carlos Audre <carlosaudre180@gmail.com>
2025-09-21 13:51:21 -0300

    PedidoController e ProdutoController adicionado

--- src/main/java/com/example/lanchonete/controller/PedidoController.java
+++ src/main/java/com/example/lanchonete/controller/PedidoController.java
@@ -1,4 +1,53 @@
 package com.example.lanchonete.controller;
 
+import java.util.List;
+import com.example.lanchonete.model.Pedido;
+import com.example.lanchonete.model.PedidoDTO;
+import com.example.lanchonete.model.Status;
+import com.example.lanchonete.service.PedidoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/pedidos")
 public class PedidoController {
+    @Autowired
+    PedidoService pedidoService;
+
+    @PostMapping
+    public ResponseEntity<Pedido> criarPedido(@RequestBody PedidoDTO dto){
+        Pedido pedidoNovo = pedidoService.criarPedido(dto);
+        return ResponseEntity.ok(pedidoNovo);
+    }
+
+    @PutMapping("/{id}/status")
+    public ResponseEntity<Pedido> atualizarStatus(@PathVariable Long id, @RequestParam Status status){
+        Pedido pedidoAtualizado = pedidoService.atualizarStatus(id, status);
+        return ResponseEntity.ok(pedidoAtualizado);
+    }
+
+    @PatchMapping("/{pedidoId}/itens/{itemId}/adicionar")
+    public ResponseEntity<Pedido> adicionarItem(@PathVariable Long pedidoId, @PathVariable Long itemId){
+        Pedido pedidoAtualizado = pedidoService.adicionarOuRemoverItem(pedidoId, itemId, true);
+        return ResponseEntity.ok(pedidoAtualizado);
+    }
+
+    @PatchMapping("/{pedidoId}/itens/{itemId}/remover")
+    public ResponseEntity<Pedido> removerItem(@PathVariable Long pedidoId, @PathVariable Long itemId){
+        Pedido pedidoAtualizado = pedidoService.adicionarOuRemoverItem(pedidoId, itemId, false);
+        return ResponseEntity.ok(pedidoAtualizado);
+    }
+
+    @GetMapping("/status")
+    public List<Pedido> filtrarPorStatus(@RequestParam Status status){
+        return pedidoService.filtrarPorStatus(status);
+    }
+
+    @DeleteMapping("{id}")
+    public void cancelarPedido(@PathVariable Long id){
+        pedidoService.cancelarPedido(id);
+    }
+
+
 }
--- src/main/java/com/example/lanchonete/controller/ProdutoController.java
+++ src/main/java/com/example/lanchonete/controller/ProdutoController.java
@@ -1,4 +1,54 @@
 package com.example.lanchonete.controller;
 
+
+import com.example.lanchonete.model.Categoria;
+import com.example.lanchonete.model.Produto;
+import com.example.lanchonete.model.ProdutoDTO;
+import com.example.lanchonete.service.ProdutoService;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/produtos")
 public class ProdutoController {
+
+    @Autowired
+    private ProdutoService produtoService;
+
+    @PostMapping
+    public ResponseEntity<Produto> registrarProduto(@RequestBody ProdutoDTO dto){
+        Produto salvo = produtoService.registrarUmProduto(dto);
+        return ResponseEntity.ok(salvo);
+    }
+
+    @GetMapping
+    public List<Produto> listarProdutos(){
+        return produtoService.listarOsProdutos();
+    }
+
+
+    @GetMapping("/categoria/{categoria}")
+    public List<Produto> listarPorCategoria(@PathVariable Categoria categoria){ //Pega o valor a url
+        return produtoService.listarPorCategoria(categoria);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Produto> atualizarProduto(@PathVariable Long id, @RequestBody ProdutoDTO dto){
+        Produto atualizado = produtoService.atualizarProduto(id, dto);
+        if(atualizado != null){
+            return ResponseEntity.ok(atualizado);
+        }
+        else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+
+    @DeleteMapping("/{id}")
+    public void deletarProduto(@PathVariable Long id){
+        produtoService.deletarProduto(id);
+    }
 }
